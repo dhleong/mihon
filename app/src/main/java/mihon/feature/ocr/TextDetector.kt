189133c6e3022b009f785595ebb13f30a26a03d3
Daniel Leong <me@dhleong.net>
2025-04-28 10:21:09 -0400

    Attempt to pre-emptively detect text on navigate
    Relies on an update to manga-ocr-android that reduces duplicate regions.
    Might be nice to also scan ahead to next pages...
    
    Another nice feature here is we can release the bitmap when we know
    we've fully scanned the page, potentially slightly easing memory
    constraint concerns.
    
    There's an edge case here where we navigate away from a page too
    quickly, but that's what the LRU cache is for.

--- app/src/main/java/mihon/feature/ocr/TextDetector.kt
+++ app/src/main/java/mihon/feature/ocr/TextDetector.kt
@@ -1,10 +1,11 @@
 package mihon.feature.ocr
 
 import android.graphics.Bitmap
+import android.graphics.RectF
 import android.util.Log
 import android.view.View
+import androidx.annotation.MainThread
 import androidx.collection.LruCache
-import androidx.core.view.drawToBitmap
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleCoroutineScope
 import coil3.imageLoader
@@ -16,7 +17,6 @@ import coil3.request.crossfade
 import coil3.size.Precision
 import coil3.size.ViewSizeResolver
 import coil3.toBitmap
-import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView
 import eu.kanade.tachiyomi.data.coil.cropBorders
 import eu.kanade.tachiyomi.data.coil.customDecoder
 import eu.kanade.tachiyomi.source.model.Page
@@ -24,11 +24,17 @@ import eu.kanade.tachiyomi.ui.reader.ReaderActivity
 import eu.kanade.tachiyomi.ui.reader.model.ReaderPage
 import eu.kanade.tachiyomi.ui.reader.viewer.ReaderPageImageView
 import eu.kanade.tachiyomi.ui.reader.viewer.pager.PagerConfig
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.cancelChildren
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.transformWhile
 import kotlinx.coroutines.launch
-import net.dhleong.mangaocr.Detector
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import net.dhleong.mangaocr.DetectorManager
 import net.dhleong.mangaocr.MangaOcr
 import net.dhleong.mangaocr.MangaOcrManager
@@ -40,11 +46,24 @@ class TextDetector(
     activity: ReaderActivity,
     private val scope: LifecycleCoroutineScope,
     lifecycle: Lifecycle,
+    private val language: String = "jp" // TODO
 ) {
-    private class PageState(
-        val bitmap: Bitmap,
-        val regions: List<Detector.Result>,
+    companion object {
+        private val SUPPORTED_LANGUAGES = setOf("jp")
+    }
+
+    private class RegionState(
+        val rect: RectF,
+        val ocr: MutableStateFlow<MangaOcr.Result?> = MutableStateFlow(null),
     )
+    private sealed interface PageState {
+        val regions: List<RegionState>
+        class FullyScanned(override val regions: List<RegionState>) : PageState
+        class Pending(
+            val bitmap: Bitmap,
+            override val regions: List<RegionState>
+        ) : PageState
+    }
 
     private val context = activity.applicationContext
     private val viewModel = activity.viewModel
@@ -52,11 +71,18 @@ class TextDetector(
     private val detection = DetectorManager(context, scope, lifecycle)
 
     private val job = SupervisorJob()
+    private var lastActiveJob: Job? = null
+    private val eagerDetectLock = Mutex()
 
     private var currentPage: ReaderPage? = null
     private val states = LruCache<ReaderPage, PageState>(4)
 
+    @MainThread
     fun detectText(view: View, viewX: Float, viewY: Float): Boolean {
+        if (language !in SUPPORTED_LANGUAGES) {
+            return false
+        }
+
         var x = viewX
         var y = viewY
         if (view is ReaderPageImageView) {
@@ -70,7 +96,7 @@ class TextDetector(
         }
 
         val state = states[currentPage ?: return false] ?: return false
-        val region = state.regions.find { it.bbox.rect.contains(x, y) }
+        val region = state.regions.find { it.rect.contains(x, y) }
         if (region == null) {
             Log.v("TextDetector", "Tap ($x, $y) did not match any region in ${state.regions}")
             return false
@@ -78,55 +104,87 @@ class TextDetector(
 
         Log.v("TextDetector", "Launching analysis at $x, $y")
 
-        job.cancelChildren()
+        lastActiveJob?.cancelChildren()
+        lastActiveJob = scope.launch(job) {
+            coroutineScope {
+                val cancel = { this@coroutineScope.cancel() }
+                eagerDetectLock.withLock {
+                    performOcr(state, region) { value ->
+                        when (value) {
+                            is MangaOcr.Result.Partial -> viewModel.updateDetectingText(
+                                RecognizedText(
+                                    text = value.text.toString(),
+                                    language = language
+                                ),
+                                cancel
+                            )
 
-        val cancel = { job.cancelChildren() }
-
-        scope.launch(job) {
-            val croppedBitmap = Bitmap.createBitmap(
-                state.bitmap,
-                region.bbox.rect.left.toInt(),
-                region.bbox.rect.top.toInt(),
-                region.bbox.rect.width().toInt(),
-                region.bbox.rect.height().toInt(),
-            )
-
-            // Show progress right away
-            viewModel.updateDetectingText(RecognizedText(
-                text = "",
-                language = "jp"
-            ), cancel)
-
-            ocr.process(croppedBitmap)
-                .collect { value ->
-                    when (value) {
-                        is MangaOcr.Result.Partial ->
-                            viewModel.updateDetectingText(RecognizedText(
-                                text = value.text.toString(),
-                                language = "jp"
-                            ), cancel)
-                        is MangaOcr.Result.FinalResult ->
-                            viewModel.finishDetectingText(RecognizedText(
-                                text = value.text,
-                                language = "jp"
-                            ))
+                            is MangaOcr.Result.FinalResult -> viewModel.finishDetectingText(
+                                RecognizedText(
+                                    text = value.text,
+                                    language = language
+                                )
+                            )
+                        }
                     }
                 }
-
-            if (croppedBitmap !== state.bitmap) {
-                croppedBitmap.recycle()
             }
         }
         return true
     }
 
+    private suspend fun performOcr(
+        page: PageState,
+        region: RegionState,
+        onEmit: (MangaOcr.Result) -> Unit = {},
+    ) {
+        if (region.ocr.value != null) {
+            // Reuse existing value/follow existing flow
+            region.ocr.transformWhile { state ->
+                if (state != null) {
+                    emit(state)
+                }
+                state !is MangaOcr.Result.FinalResult
+            }.collect(onEmit)
+            return
+        }
+
+        val bitmap = (page as? PageState.Pending)?.bitmap
+            ?: throw IllegalStateException("FullyScanned page should not have any null OCR values")
+
+        val croppedBitmap = Bitmap.createBitmap(
+            page.bitmap,
+            region.rect.left.toInt(),
+            region.rect.top.toInt(),
+            region.rect.width().toInt(),
+            region.rect.height().toInt(),
+        )
+
+        // Show progress right away
+        onEmit(MangaOcr.Result.Partial(""))
+
+        ocr.process(croppedBitmap)
+            .collect { value ->
+                region.ocr.value = value
+                onEmit(value)
+            }
+
+        if (croppedBitmap !== bitmap) {
+            croppedBitmap.recycle()
+        }
+    }
+
     fun onPageSelected(config: PagerConfig, view: View, page: ReaderPage) {
         Log.v("TextDetector", "onPageSelected ($page / ${page.number})")
         currentPage = page
-        scanPageForRegions(config, view, page)
+
+        if (language in SUPPORTED_LANGUAGES) {
+            scanPageForRegions(config, view, page)
+        }
     }
 
-    fun scanPageForRegions(config: PagerConfig, view: View, page: ReaderPage) {
+    private fun scanPageForRegions(config: PagerConfig, view: View, page: ReaderPage) {
+        // NOTE: This function *could* be public to pre-scan... but may not be necessary
         scope.launchUI {
             val existing = states[page]
             Log.v("TextDetector", "scanPageForRegions ($page / ${page.number}) existing = $existing")
@@ -141,9 +199,31 @@ class TextDetector(
             }
 
             Log.v("TextDetector", "Process ($page / ${page.number}) @ ${bitmap.width} / ${bitmap.height}...")
-            val results = detection.process(bitmap)
+            val results = detection.process(bitmap).map {
+                RegionState(it.bbox.rect)
+            }
             Log.v("TextDetector", "Got: $results")
-            states.put(page, PageState(bitmap, results))
+            val state = PageState.Pending(bitmap, results)
+            states.put(page, state)
+
+            // Eagerly detect text
+            for (region in state.regions) {
+                val currentPageNumber = currentPage?.number ?: 0
+                if (currentPageNumber > page.number) {
+                    Log.v("TextDetector", "Stopping early; $currentPageNumber > ${page.number}")
+                    break
+                }
+
+                eagerDetectLock.withLock {
+                    performOcr(state, region)
+                }
+            }
+
+            // NOTE: By switching to a FullyScanned we release the reference to the bitmap
+            // we loaded, enabling the memory to be freed. We *may* be able to eagerly recycle(),
+            // but keeping this simple for now to avoid accidents---the GC should catch it
+            Log.v("TextDetector", "Fully scanned $page / ${page.number}")
+            states.put(page, PageState.FullyScanned(results))
         }
     }
 
